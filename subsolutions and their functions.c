23c9d9c393957f2a4cd6a566fe9cd4d71455618b
Batwega <139142931+Batwega@users.noreply.github.com>
2023-07-13 09:10:21 -0700

    added file via vsc

---
+++ subsolutions and their functions.c
@@ -0,0 +1,6 @@
+
+Functions:
+1. expandRoadCapacity():
+   - Parameters: road (string), additionalLanes (int)
+   - Description: Increases the number of lanes on a congested road.
+   - Variables: roadCapacity (int), updatedRoadCapacity (int)
